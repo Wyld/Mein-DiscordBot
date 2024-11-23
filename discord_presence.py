e41dc7a61676847636ee2f372695262648f3df31
Julien . <Julien301.strassburg@gmail.comgit config --global user.email Julien301.strassburg@gmail.comgit config --global user.name Wyldgit commit -m Add>
2024-11-23 22:19:50 +0100

    Aktualisere discord_presence.py

--- discord_presence.py
+++ discord_presence.py
@@ -1,7 +1,7 @@
 import discord
 
 async def update_presence(bot: discord.Client):
-    activity = discord.Game(name="Spielt mit Feelings - Competitive")
+    activity = discord.Game(name="mit Feelings - Competitive")
     print("Aktualisiere Präsenz...")
     await bot.change_presence(activity=activity)
     print("Präsenz aktualisiert.")
